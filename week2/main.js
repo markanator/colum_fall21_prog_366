bd2743f8edcc772e7f25c4fa41b562906ae98a9e
Mark Ambrocio <wowmarkambro@gmail.com>
2021-09-15 15:18:28 -0600

    added comments

--- week2/main.js
+++ week2/main.js
@@ -1,17 +1,26 @@
 //* 2.9: Assignment I: Code Examples and Portfolio Start
+
 //Constant O(1)
 function returnArrayIndexValue(array, key) {
+  // we know where in the array we want the element
   return array[key];
 }
+
 // Linear O(n)
 function greetWorld(timesToGreet) {
+  // timesToGreet can be any positive number, under 9,007,199,254,740,991
+  if (timesToGreet < 0 || timesToGreet > Number.MAX_SAFE_INTEGER) return;
+  // so we must loop until we reach meet the condition in the loop
   for (let i = 0; i < timesToGreet; i++) {
     console.log("Hello, world!");
   }
 }
+
 // Quadratic O(n2)
 function nestedLoop(mdArray) {
+  // mdArray is an Array of arrays
   mdArray.forEach((el) => {
+    // each el is an Array and we want to loop and print that element
     el.forEach((item) => {
       console.log(item);
     });
